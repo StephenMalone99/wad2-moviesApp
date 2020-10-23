1e06e73de17f7023120551362019ca4f226e8019
Stephen Malone <stephenmalone99@hotmail.com>
2020-10-23 15:58:16 +0100

    Added routing and live data from API

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
 import Header from "../components/headerMovieList";
 import MovieList from "../components/movieList";
 import FilterControls from "../components/filterControls";
 
-const MovieListPage = ({movies}) => {
+const MovieListPage = () => {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    fetch(
+      'https://api.themoviedb.org/3/discover/movie?api_key=d8722c355bec01520ff099a3e01c203e'
+    )
+      .then(res => res.json())
+      .then(json => {
+        console.log(json)
+        return json.results
+      })
+      .then(movies => {
+        setMovies(movies);
+      });
+  }, []);
   return (
     <>
       <Header numMovies={movies.length} />
--- src/pages/movieDetailsPage.js
+++ src/pages/movieDetailsPage.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, {useState, useEffect}  from "react";
 import MovieHeader from "../components/headerMovie";
 import MovieDetails from "../components/movieDetails";
 import "./moviePage.css";
 
-const MoviePage = ({ movie }) => {
+const MoviePage = props => {
+  const { id } = props.match.params
+  const [movie, setMovie] = useState(null)
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=d8722c355bec01520ff099a3e01c203e`
+    ).then(res => res.json())    
+    .then(movie => {
+      setMovie(movie);
+    })
+  }, [id])
+
   return (
     <>
       {movie ? (
